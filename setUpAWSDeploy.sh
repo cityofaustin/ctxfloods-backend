5b02721dbe234e312f2e5cfa8a75b8816e5c8993
Brian Smith <briaguya@gmail.com>
2018-03-22 15:09:24 -0500

    update set up aws script

--- setUpAWSDeploy.sh
+++ setUpAWSDeploy.sh
@@ -1,7 +1,4 @@
 export CURRENT_FLOODS_BRANCH_NAME=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-
-export S3_BUCKET=$(echo "ctxfloods-frontend-$CURRENT_FLOODS_BRANCH_NAME" | tr '[:upper:]' '[:lower:]')
-
 export npm_config_PGCON=""
 export npm_config_PGRUNCON=""
 
@@ -20,10 +17,10 @@ export AWS_SECRET_ACCESS_KEY
 travis encrypt AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY --add
 
 echo "Frontend URL for reset email links"
-read -s FRONTEND_URL
+read FRONTEND_URL
 export FRONTEND_URL
 
-cho "Generating JWT secret"
+echo "Generating JWT secret"
 export JWT_SECRET=$(openssl rand -base64 32)
 travis encrypt JWT_SECRET=$JWT_SECRET --add
 
@@ -31,7 +28,6 @@ tput bold
 echo "Deploying to AWS to get a cloudformation/endpoint"
 tput sgr0
 
-cd backend
 yarn
 sls deploy -v | tee out.tmp
 export PGENDPOINT=$(grep "pgEndpoint" out.tmp | cut -f2- -d: | cut -c2-)
@@ -42,26 +38,19 @@ echo "Setting PGCON and PGRUNCON"
 tput sgr0
 
 export npm_config_PGCON=$(echo postgresql://example:serverless@$PGENDPOINT:5432/floods)
-travis encrypt npm_config_PGCON=$npm_config_PGCON --add
+echo "  - npm_config_PGCON=$npm_config_PGCON" >> .travis.yml
 export npm_config_PGRUNCON=$(echo postgresql://floods_postgraphql:xyz@$PGENDPOINT:5432/floods)
-travis encrypt npm_config_PGRUNCON=$npm_config_PGRUNCON --add
+echo "  - npm_config_PGRUNCON=$npm_config_PGRUNCON" >> .travis.yml
 
 tput bold 
 echo "Deploying to AWS"
 tput sgr0
 
 yarn rebuild-and-deploy | tee out.tmp
-export POSTGRAPHQL_ENDPOINT=$(grep "POST" out.tmp | cut -f2- -d- | cut -c2-)
-export XML_ENDPOINT=$(grep "GET.*xml" out.tmp | cut -f2- -d- | cut -c2-)
-export EMAIL_ENDPOINT=$(grep "POST.*email/reset" out.tmp | cut -f2- -d- | cut -c2-)
+
+export POSTGRAPHQL_ENDPOINT=$(grep "POST.*graphql" out.tmp | cut -f2- -d- | cut -c2-)
+echo "  - POSTGRAPHQL_ENDPOINT=$POSTGRAPHQL_ENDPOINT" >> .travis.yml
 
 rm out.tmp
-travis encrypt POSTGRAPHQL_ENDPOINT=$POSTGRAPHQL_ENDPOINT --add
-travis encrypt REACT_APP_GRAPHQL_ENDPOINT=$POSTGRAPHQL_ENDPOINT --add
-travis encrypt REACT_APP_XML_ENDPOINT=$XML_ENDPOINT --add
-travis encrypt REACT_APP_EMAIL_ENDPOINT=$EMAIL_ENDPOINT --add
 
-cd ..
-echo "  - CURRENT_FLOODS_BRANCH_NAME=$CURRENT_FLOODS_BRANCH_NAME" >> .travis.yml
-echo "  - S3_BUCKET=$S3_BUCKET" >> .travis.yml
 echo "  - FRONTEND_URL=$FRONTEND_URL" >> .travis.yml
