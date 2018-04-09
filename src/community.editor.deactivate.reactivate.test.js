75f5a2d3d94f9b13c65a2570d6f8ede3581f8f4f
Brian Smith <briaguya@gmail.com>
2018-04-09 12:37:20 -0500

    another test fix

--- src/community.editor.deactivate.reactivate.test.js
+++ src/community.editor.deactivate.reactivate.test.js
@@ -169,10 +169,7 @@ describe('When deactivating, and reactivating a user as a community editor', () 
           `
           mutation($userId:Int!, $email:String!) {
             reactivateUser(input: {
-              userId: $userId,
-              email: $email
-              password: "texasfloods"
-              role: "floods_community_editor"
+              userId: $userId
             }) {
               user {
                 firstName
@@ -183,8 +180,7 @@ describe('When deactivating, and reactivating a user as a community editor', () 
           }
         `,
           {
-            userId: newUserId,
-            email: newUserEmail,
+            userId: newUserId
           },
         );
       } catch (e) {
