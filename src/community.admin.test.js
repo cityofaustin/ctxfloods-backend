3409d1ee8dd974d5afe0f155933c5f0b4218fd30
Brian Smith <briaguya@gmail.com>
2018-04-10 06:27:20 -0500

    remove outdated test

--- src/community.admin.test.js
+++ src/community.admin.test.js
@@ -66,38 +66,6 @@ describe('As a community admin', async () => {
     }
   });
 
-  it('should fail to delete a user from another community', async () => {
-    try {
-      const response = await lokka.send(`
-      mutation {
-        removeUser(input: {userId: 4}) {
-          user {
-            id
-          }
-        }
-      }      
-      `);
-    } catch (e) {
-      expect(e).toMatchSnapshot();
-    }
-  });
-
-  it('should fail to delete a crossing from another community', async () => {
-    try {
-      const response = await lokka.send(`
-      mutation {
-        removeCrossing(input: {crossingId: 8}) {
-          crossing {
-            id
-          }
-        }
-      }      
-      `);
-    } catch (e) {
-      expect(e).toMatchSnapshot();
-    }
-  });
-
   it('should fail to update the status of a crossing from another community', async () => {
     try {
       const response = await lokka.send(`
