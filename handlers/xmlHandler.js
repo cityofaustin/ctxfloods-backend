1445650366848a72514969b0d72a22d15aa3e4d3
Luqmaan <ldawoodjee@gmail.com>
2018-04-23 11:50:41 -0500

    Add serverless-sentry plugin
    To test that it works I ran:
    
    ```
    CURRENT_FLOODS_BRANCH_NAME=CURRENT_FLOODS_BRANCH_NAME \
    FRONTEND_URL=FRONTEND_URL \
    GMAIL_ADDRESS=GMAIL_ADDRESS \
    GMAIL_PASSWORD=GMAIL_PASSWORD \
    JWT_SECRET=JWT_SECRET \
    PGPASSWORD=PGPASSWORD \
    PGUSERNAME=PGUSERNAME \
    SENTRY_DSN=https://9896d78192714c05a9e65bfd2e6e9be3@sentry.io/1194428 \
    npm_config_PGRUNCON=npm_config_PGRUNCON \
    SLS_DEBUG=* \
    SENTRY_FILTER_LOCAL=false \
    serverless invoke local -f emailreset
    ```
    
    Note the SENTRY_FILTER_LOCAL variable. I had issues with the filterLocal option:  https://github.com/arabold/serverless-sentry-lib/issues/8.
    
    Sentry is setup through the heroku project `floods-backend-sentry`.
    
    Sentry url: https://addons-sso.heroku.com/apps/229671e9-62d1-4800-a6d1-657410ccb2a0/addons/005b629e-ae27-4ad7-a554-ce0dace709ce
    
    https://github.com/cityofaustin/ctxfloods/issues/154

--- handlers/xmlHandler.js
+++ handlers/xmlHandler.js
@@ -1,22 +1,27 @@
 const Client = require('pg').Client;
+const Raven = require('raven');
+const RavenLambdaWrapper = require('serverless-sentry-lib');
 
-module.exports.handle = (event, context, cb) => {
-  const pgClient = new Client(process.env.PGCON);
-  pgClient.connect();
+module.exports.handle = RavenLambdaWrapper.handler(
+  Raven,
+  (event, context, cb) => {
+    const pgClient = new Client(process.env.PGCON);
+    pgClient.connect();
 
-  pgClient.query('select floods.legacy_xml()')
-    .then(pgres => 
-    {
-      const response = {
+    pgClient
+      .query('select floods.legacy_xml()')
+      .then(pgres => {
+        const response = {
           statusCode: 200,
           headers: {
             'Content-Type': 'text/xml',
           },
           body: pgres.rows[0].legacy_xml,
-      };
+        };
 
-      cb(null, response);
-    })
-    .catch(err => cb(null, {"errors": [err]}))
-    .then(() => pgClient.end());    
-}
+        cb(null, response);
+      })
+      .catch(err => cb(null, { errors: [err] }))
+      .then(() => pgClient.end());
+  },
+);
