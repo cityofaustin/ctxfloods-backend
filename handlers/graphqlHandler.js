679f10107748ab9977b9ef90e1ca140e63c33489
Luqmaan <ldawoodjee@gmail.com>
2018-04-24 18:03:35 -0500

    Actually actually remove sentry

--- handlers/graphqlHandler.js
+++ handlers/graphqlHandler.js
@@ -2,8 +2,6 @@
 const fs = require('fs');
 const graphql = require('graphql').graphql;
 const Client = require('pg').Client;
-const Raven = require('raven');
-const RavenLambdaWrapper = require('serverless-sentry-lib');
 
 const PgCatalogBuilder = require('postgraphql/build/postgres/introspection/PgCatalog');
 const createPostGraphQLSchema = require('postgraphql/build/postgraphql/schema/createPostGraphQLSchema');
@@ -11,71 +9,68 @@ const pgClientFromContext = require('postgraphql/build/postgres/inventory/pgClie
 const setupRequestPgClientTransaction = require('postgraphql/build/postgraphql/http/setupRequestPgClientTransaction');
 const PgCat = JSON.parse(fs.readFileSync('pgCatalog/pgCatalog.json', 'utf8'));
 
-module.exports.handle = RavenLambdaWrapper.handler(
-  Raven,
-  (event, context, cb) => {
-    // Setup connection to PostgresDB
-    const pgClient = new Client(process.env.PGCON);
-    pgClient.connect();
+module.exports.handle = (event, context, cb) => {
+  // Setup connection to PostgresDB
+  const pgClient = new Client(process.env.PGCON);
+  pgClient.connect();
 
-    // Parse PgCatalog
-    const PgCatalog = new PgCatalogBuilder.default(PgCat);
+  // Parse PgCatalog
+  const PgCatalog = new PgCatalogBuilder.default(PgCat);
 
-    // Set postgraphql options
-    // For all options see https://github.com/calebmer/postgraphql/blob/master/docs/library.md
-    const options = {
-      pgDefaultRole: 'floods_anonymous',
-      jwtSecret: process.env.JWT_SECRET,
-      jwtPgTypeIdentifier: 'floods.jwt_token',
-      pgDefaultRole: 'floods_anonymous',
-      disableDefaultMutations: true,
-    };
-    let gqlSchema;
-    createPostGraphQLSchema
-      .default(pgClient, PgCatalog, options)
-      .then(schema => {
-        try {
-          gqlSchema = schema;
-          // To be honest i am not 100% that the following
-          // proppery uses 'begin' ... 'commit'
-          pgClient.query('begin').then(() => {
-            setupRequestPgClientTransaction
-              .default(event, pgClient, {
-                jwtSecret: options.jwtSecret,
-                pgDefaultRole: options.pgDefaultRole,
-              })
-              .then(pgRole => {
-                graphql(
-                  gqlSchema,
-                  event.query,
-                  null,
-                  { [pgClientFromContext.$$pgClient]: pgClient },
-                  event.variables,
-                  event.operationName,
-                )
-                  .then(response => {
-                    pgClient.end();
+  // Set postgraphql options
+  // For all options see https://github.com/calebmer/postgraphql/blob/master/docs/library.md
+  const options = {
+    pgDefaultRole: 'floods_anonymous',
+    jwtSecret: process.env.JWT_SECRET,
+    jwtPgTypeIdentifier: 'floods.jwt_token',
+    pgDefaultRole: 'floods_anonymous',
+    disableDefaultMutations: true,
+  };
+  let gqlSchema;
+  createPostGraphQLSchema
+    .default(pgClient, PgCatalog, options)
+    .then(schema => {
+      try {
+        gqlSchema = schema;
+        // To be honest i am not 100% that the following
+        // proppery uses 'begin' ... 'commit'
+        pgClient.query('begin').then(() => {
+          setupRequestPgClientTransaction
+            .default(event, pgClient, {
+              jwtSecret: options.jwtSecret,
+              pgDefaultRole: options.pgDefaultRole,
+            })
+            .then(pgRole => {
+              graphql(
+                gqlSchema,
+                event.query,
+                null,
+                { [pgClientFromContext.$$pgClient]: pgClient },
+                event.variables,
+                event.operationName,
+              )
+                .then(response => {
+                  pgClient.end();
 
-                    response.statusCode = 200;
-                    response.headers = { 'Access-Control-Allow-Origin': '*' };
-                    cb(null, response);
-                  })
-                  .catch(() => cb(e));
-              })
-              .then(() => pgClient.query('commit'))
-              .catch(err => {
-                pgClient.end();
-                cb(null, { errors: [err] });
-              });
-          });
-        } catch (err) {
-          pgClient.end();
-          cb(null, { errors: [err] });
-        }
-      })
-      .catch(err => {
+                  response.statusCode = 200;
+                  response.headers = { 'Access-Control-Allow-Origin': '*' };
+                  cb(null, response);
+                })
+                .catch(() => cb(e));
+            })
+            .then(() => pgClient.query('commit'))
+            .catch(err => {
+              pgClient.end();
+              cb(null, { errors: [err] });
+            });
+        });
+      } catch (err) {
         pgClient.end();
         cb(null, { errors: [err] });
-      });
-  },
-);
+      }
+    })
+    .catch(err => {
+      pgClient.end();
+      cb(null, { errors: [err] });
+    });
+};
--- handlers/xmlHandler.js
+++ handlers/xmlHandler.js
@@ -1,27 +1,22 @@
 const Client = require('pg').Client;
-const Raven = require('raven');
-const RavenLambdaWrapper = require('serverless-sentry-lib');
 
-module.exports.handle = RavenLambdaWrapper.handler(
-  Raven,
-  (event, context, cb) => {
-    const pgClient = new Client(process.env.PGCON);
-    pgClient.connect();
+module.exports.handle = (event, context, cb) => {
+  const pgClient = new Client(process.env.PGCON);
+  pgClient.connect();
 
-    pgClient
-      .query('select floods.legacy_xml()')
-      .then(pgres => {
-        const response = {
-          statusCode: 200,
-          headers: {
-            'Content-Type': 'text/xml',
-          },
-          body: pgres.rows[0].legacy_xml,
-        };
+  pgClient
+    .query('select floods.legacy_xml()')
+    .then(pgres => {
+      const response = {
+        statusCode: 200,
+        headers: {
+          'Content-Type': 'text/xml',
+        },
+        body: pgres.rows[0].legacy_xml,
+      };
 
-        cb(null, response);
-      })
-      .catch(err => cb(null, { errors: [err] }))
-      .then(() => pgClient.end());
-  },
-);
+      cb(null, response);
+    })
+    .catch(err => cb(null, { errors: [err] }))
+    .then(() => pgClient.end());
+};
