d75022792a3a55ee296f0fdade6ce0dfe24fd947
Luqmaan <ldawoodjee@gmail.com>
2018-06-21 12:52:24 -0500

    Transform incident feed shape
    Add test, but its not in the src directory so it doesn't work.
    
    I can't get `yarn test` to run properly, so that needs to be tackled first.

--- handlers/wazeFeedHandler.js
+++ handlers/wazeFeedHandler.js
@@ -1,5 +1,25 @@
 const Client = require('pg').Client;
 
+function getPgResAsIncidentJson(rows) {
+  return rows;
+  return {
+    incidents: rows.map(({waze_feed: row}) => ({
+      id: row.id,
+      location: {
+        street: row.street,
+        polyline: row.polyline,
+        direction: row.direction,
+      },
+      type: row.type,
+      subtype: row.subtype,
+      starttime: row.starttime,
+      description: row.description,
+      reference: row.reference,
+    })),
+  };
+}
+module.exports.getPgResAsIncidentJson = getPgResAsIncidentJson;
+
 module.exports.handle = (event, context, cb) => {
   const pgClient = new Client(process.env.PGCON);
   pgClient.connect();
@@ -7,12 +27,14 @@ module.exports.handle = (event, context, cb) => {
   pgClient
     .query('select floods.waze_feed()')
     .then(pgres => {
+      const incidentJson = getPgResAsIncidentJson(pgres.rows);
+
       const response = {
         statusCode: 200,
         headers: {
           'Content-Type': 'application/json',
         },
-        body: pgres.rows,
+        body: incidentJson,
       };
 
       cb(null, response);
