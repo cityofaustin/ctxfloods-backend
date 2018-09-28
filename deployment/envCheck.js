9f901d1a93f7e1962742751210dfc8621002b7bd
Niicck <niicck@users.noreply.github.com>
2018-09-28 11:36:04 -0500

    fix env variable check

--- deployment/envCheck.js
+++ deployment/envCheck.js
@@ -1,13 +1,14 @@
 const envCheck = () => {
   console.log("Hey lets look at all our varzzz");
-  console.log(`AWS_SERVICE_NAME: ${AWS_SERVICE_NAME}`);
-  console.log(`FRONTEND_URL: ${FRONTEND_URL}`);
-  console.log(`GMAIL_ADDRESS: ${GMAIL_ADDRESS}`);
-  console.log(`JWT_SECRET: ${JWT_SECRET.length}`);
-  console.log(`PGUSERNAME: ${PGUSERNAME}`);
-  console.log(`PGPASSWORD: ${PGPASSWORD}`);
+  console.log(`AWS_SERVICE_NAME: ${process.env.AWS_SERVICE_NAME}`);
+  console.log(`FRONTEND_URL: ${process.env.FRONTEND_URL}`);
+  console.log(`GMAIL_ADDRESS: ${process.env.GMAIL_ADDRESS}`);
+  console.log(`JWT_SECRET: ${process.env.JWT_SECRET ? process.env.JWT_SECRET.length : 'nada'}`);
+  console.log(`PGUSERNAME: ${process.env.PGUSERNAME}`);
+  console.log(`PGPASSWORD: ${process.env.PGPASSWORD}`);
   console.log("And now the big one");
-  console.log(`PGCON: ${PGCON}`);
+  console.log(`PGCON: ${process.env.PGCON}`);
+  console.log(`PGCON: ${process.env.ONLY_IN_SERVERLESS}`);
 }
 
 module.exports = envCheck;
