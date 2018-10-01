cd91611e4fbd92f664a163c710c0c61efc7e1241
Niicck <niicck@users.noreply.github.com>
2018-09-30 23:06:15 -0500

    PG_CON working in serverless, testing multiple deployers

--- handlers/xmlHandler.js
+++ handlers/xmlHandler.js
@@ -2,7 +2,7 @@ const Client = require('pg').Client;
 const { logError } = require('./logger');
 
 module.exports.handle = (event, context, cb) => {
-  const pgClient = new Client(process.env.PGCON);
+  const pgClient = new Client(process.env.PG_CON);
   pgClient.connect();
 
   pgClient
