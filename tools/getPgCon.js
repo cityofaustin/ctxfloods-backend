61312aaae69bac4f0906f36e46935a223f886608
Niicck <niicck@users.noreply.github.com>
2018-10-02 16:42:26 -0500

    return to PGENDPOINT var

--- tools/getPgCon.js
+++ tools/getPgCon.js
@@ -2,6 +2,6 @@ module.exports = () => {
   if (process.env.PGCON) {
     return process.env.PGCON; // For local testing
   } else {
-    return `postgresql://floods_postgraphql:xyz@${process.env.PG_ENDPOINT}:5432/floods`;
+    return `postgresql://floods_postgraphql:xyz@${process.env.PGENDPOINT}:5432/floods`;
   }
 }
