b44ceba60c7a01b87721b1f14103f231fdbd96d8
Niicck <niicck@users.noreply.github.com>
2018-10-18 10:50:25 -0500

    finalize travis env variable naming convention

--- deployment/vars/local.sh
+++ deployment/vars/local.sh
@@ -9,6 +9,7 @@ export AWS_STAGE='dev'
 export JWT_SECRET=insecure
 export FRONTEND_URL="localhost:3000"
 export BACKEND_PORT=5000
+# ENABLE_SYNC_LEGACY not required for local testing
 export GRAPHQL_ENDPOINT=http://localhost:$BACKEND_PORT/graphql
 
 # Postgres
