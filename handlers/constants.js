e75fee5493c3b573bdedf71039fd1d2b107fed42
Niicck <niicck@users.noreply.github.com>
2018-11-16 12:14:20 -0600

    fix serverless env var config for pushNotifications

--- handlers/constants.js
+++ handlers/constants.js
@@ -7,6 +7,8 @@ module.exports.PGCON =
 module.exports.PGCON_BUILD_SCHEMA =
   `postgresql://${process.env.PGUSERNAME}:${process.env.PGPASSWORD}@${process.env.PGENDPOINT}:5432/floods`
 
+// Only used in pushNotificationHandler, which is a cron job that does not receive a request from a frontend.
+// Otherwise, it's more accurate to use event.headers.origin to get frontendURL
 let frontendURL;
 if (process.env.AWS_SERVICE_NAME === 'ctxfloods-backend-prod-legacy-sync') {
   frontendURL = 'floods.austintexas.io';
