adc5c2a526194f286a164c59e4acfb5eab1cf9d1
Niicck <niicck@users.noreply.github.com>
2018-11-26 07:57:47 -0600

    add writeCustomServiceName deployment script

--- deployment/devDeployConfig.js
+++ deployment/devDeployConfig.js
@@ -5,8 +5,16 @@
 
   [branch-name]: {
       deploy: Boolean (indicate whether to deploy or not),
+        Defaults to false.
       seed: Boolean (indicate whether to seed data into new deployment or not)
+        Defaults to false.
       pushNotifications: Boolean, toggle sending emails to community admins when crossings need updating
+        Defaults to false
+      customServiceName: String, a custom AWS Service Name.
+        Defaults to branchName.
+        This can be used to push changes to an existing deployment that doesn't share the branch name.
+        Can push to an environment like "sandbox-1" to save deployment time.
+        If "customServiceName" is not an existing deployment, then no time will be saved because a new CloudFormation must still be created.
   }
 **/
 
@@ -23,5 +31,10 @@ module.exports = {
     deploy: true,
     seed: true,
     pushNotifications: true
+  },
+  "custom-service-name": {
+    deploy: true,
+    seed: true,
+    customServiceName: "sandbox-2"
   }
 };
