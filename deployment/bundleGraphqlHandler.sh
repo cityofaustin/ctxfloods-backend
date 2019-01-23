30fd537f652d885286b0b19ab2a13e0de00b56a5
Niicck <niicck@users.noreply.github.com>
2019-01-22 20:55:32 -0600

    add extra logs for debugging

--- deployment/bundleGraphqlHandler.sh
+++ deployment/bundleGraphqlHandler.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 CURRENT_DIR=`dirname $BASH_SOURCE`
 
-echo Building Schema
+echo "Building Schema"
 node $CURRENT_DIR/../pgCatalog/buildPgCatalog.js
 if [ $? != 0 ]; then
   echo "buildPgCatalog failed"
   exit 1
+fi
+
+echo "What is inside graphql.zip?"
+unzip -l $CURRENT_DIR/../.serverless/graphql.zip
+if [ -f $CURRENT_DIR/../pgCatalog/postgraphile.cache ]; then
+  echo "k the cache exists"
+else
+  echo "hey the cache doesn't exist"
 fi
 
 # "package: include: ..." within serverless.yml is incompatible with serverless-webpack, hence the workaround
