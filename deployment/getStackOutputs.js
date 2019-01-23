8b95a1e2c03d87d81627ed3a92eed2dd97153d5d
Niicck <niicck@users.noreply.github.com>
2019-01-22 18:15:49 -0600

    add script to check Migrations

--- deployment/getStackOutputs.js
+++ deployment/getStackOutputs.js
@@ -5,9 +5,10 @@ const cloudformation = new aws.CloudFormation();
 
 /**
   Retrieves PG_ENDPOINT and GRAPHQL_ENDPOINT stack outputs from AWS.
+  Also exports STACK_EXISTS flag to indicate whether the Stack already existed or not.
   If "strict" === true, then we throw an error if the Stack does not exist
 **/
-const getStackOutputs = (strict) => {
+const getStackOutputs = (strict=false) => {
   const stackName = `${process.env.AWS_SERVICE_NAME}-${process.env.AWS_STAGE}`;
 
   return cloudformation.describeStacks({
@@ -16,16 +17,14 @@ const getStackOutputs = (strict) => {
   .then(data => {
     const PG_ENDPOINT = data.Stacks.Outputs.find(o => o.OutputKey === "PgEndpoint").OutputValue;
     const GRAPHQL_ENDPOINT = data.Stacks.Outputs.find(o => o.OutputKey === "GraphqlEndpoint").OutputValue;
-    fs.writeFileSync(`${__dirname}/stack_outputs.tmp`, `export PG_ENDPOINT=${PG_ENDPOINT}\nexport GRAPHQL_ENDPOINT=${GRAPHQL_ENDPOINT}\n export STACK_EXISTS=true`);
+    fs.writeFileSync(`${__dirname}/stack_outputs.tmp`, `export PG_ENDPOINT=${PG_ENDPOINT}\nexport GRAPHQL_ENDPOINT=${GRAPHQL_ENDPOINT}\nexport STACK_EXISTS=true`);
   })
   .catch(err => {
-    if (err.code === "ValidationError") {
-      if (strict) {
-        console.log(err)
-        process.exit(1)
-      } else {
-        fs.writeFileSync(`${__dirname}/stack_outputs.tmp`,`export STACK_EXISTS=false`);
-      }
+    if (err.code === "ValidationError" && !strict) {
+      fs.writeFileSync(`${__dirname}/stack_outputs.tmp`,`export STACK_EXISTS=false`);
+    } else {
+      console.log(err)
+      process.exit(1)
     }
   })
 }
