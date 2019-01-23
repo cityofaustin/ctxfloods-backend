7f258a0457be1a9a7668ce3e34935b3d85df4fdb
Niicck <niicck@users.noreply.github.com>
2019-01-22 23:15:11 -0600

    remove console logs

--- deployment/bundleGraphqlHandler.sh
+++ deployment/bundleGraphqlHandler.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 CURRENT_DIR=`dirname $BASH_SOURCE`
-echo what is pwd? $(pwd)
-echo what is ls? $(ls)
 
 echo "Building Schema"
 node $CURRENT_DIR/../pgCatalog/buildPgCatalog.js
@@ -10,20 +8,11 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
-echo "What is inside graphql.zip?"
-unzip -l $CURRENT_DIR/../.serverless/graphql.zip
-if [ -f $CURRENT_DIR/../pgCatalog/postgraphile.cache ]; then
-  echo "k the cache exists"
-else
-  echo "hey the cache doesn't exist"
-fi
-
 # "package: include: ..." within serverless.yml is incompatible with serverless-webpack, hence the workaround
 # http://nmajor.com/posts/serverless-framework-executable-binaries-aws-lambda
+# Warning: this zip commands depends on a relative path.
+# If the cwd ever changes during the deployment process, you should use node-archiver for zipping instead.
 zip -9Xr $CURRENT_DIR/../.serverless/graphql.zip pgCatalog/postgraphile.cache
-
-echo "NOW whats insdie graphql.zip?"
-unzip -l $CURRENT_DIR/../.serverless/graphql.zip
 
 # Update SHA hash for modified function zip file
 export SHA=$(openssl dgst -sha256 -binary $CURRENT_DIR/../.serverless/graphql.zip | openssl enc -base64)
