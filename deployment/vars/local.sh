e4431f3faf8799fda034a7d5087af769d8c874da
Niicck <niicck@users.noreply.github.com>
2019-01-27 10:03:37 -0600

    add sudo for Travis

--- deployment/vars/local.sh
+++ deployment/vars/local.sh
@@ -32,7 +32,7 @@ export PG_API_PW=floods_graphql
 export PG_SUPER_ADMIN_PW=texasfloods # only used during initialization
 export PG_ENDPOINT=localhost
 
-# Travis postgres v10 runs on port 5433
+# Travis postgres v10 requires special configs
 if [[ $TRAVIS = true ]];then
   export PG_PORT=5433
   export PG_MASTER_USR=postgres
