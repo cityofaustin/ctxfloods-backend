9375c0c32ae755b8d12c6b1b2a39d859883ef1ff
Niicck <niicck@users.noreply.github.com>
2019-01-27 10:23:58 -0600

    try new db user for Travis

--- deployment/vars/local.sh
+++ deployment/vars/local.sh
@@ -35,6 +35,6 @@ export PG_ENDPOINT=localhost
 # Travis postgres v10 requires special configs
 if [[ $TRAVIS = true ]];then
   export PG_PORT=5433
-  export PG_MASTER_USR=postgres
-  export PG_MASTER_PW=password
+  export PG_MASTER_USR=travis
+  export PG_MASTER_PW=''
 fi
